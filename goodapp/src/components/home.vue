20f9e261c623b27793d83a4395dfa080004044c3
student <student@hubu.edu.cn>
2019-03-20 11:31:37 +0800

    gogogo

---
+++ goodapp/src/components/home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <x-header>This is the page title.</x-header>
+  </div>
+</template>
+
+<script>
+import { XHeader, Actionsheet, TransferDom, ButtonTab, ButtonTabItem } from 'vux'
+
+export default {
+  directives: {
+    TransferDom
+  },
+  components: {
+    XHeader,
+    Actionsheet,
+    ButtonTab,
+    ButtonTabItem
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+.overwrite-title-demo {
+  margin-top: 5px;
+}
+</style>
